7965ab3591771bf78565500e4233fd2f3dff49ca
DudasBenceSandor <dudasbencesandor@turr.hu>
2025-12-15 17:52:35 +0100

    hamburger menu kész specialitásaink grid elkezdése és a footer

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-const hamburger = document.querySelector(".hamburger");
-const menu = document.querySelector("nav .part_2");
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger");
+  const menu = document.querySelector("nav .part_2");
 
-hamburger.addEventListener("click", () => {
-  menu.classList.toggle("active");
+  console.log("JS fut", hamburger, menu);
+
+  hamburger.addEventListener("click", () => {
+    menu.classList.toggle("active");
+    console.log("active most:", menu.classList.contains("active"));
+  });
 });
